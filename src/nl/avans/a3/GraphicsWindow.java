c596ca581701ad947b3c91c3ca0c315f7be08c8f
Remco Korevaar <remcokorevaar@gmail.com>
2016-05-19 13:38:34 +0200

    Added final constants for height and width

--- src/nl/avans/a3/GraphicsWindow.java
+++ src/nl/avans/a3/GraphicsWindow.java
@@ -10,13 +10,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.font.GlyphVector;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class GraphicsWindow extends JPanel implements ActionListener{
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
+    
     private int startCounter = 0;
     private boolean timerstarted = false;
     Shape s = null;
