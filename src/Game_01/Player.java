47a4c27c80f557972f6d45d293260e859224bbe2
Thijs <tdrooij@gmail.com>
2016-05-19 14:14:52 +0200

    Timer in the game

--- src/Game_01/Player.java
+++ src/Game_01/Player.java
@@ -9,9 +9,9 @@ import javax.swing.Timer;
 
 public class Player extends Sprite {
 
-	int dx, dy;
-	int number;
-	boolean jump = false;
+	private int dx, dy;
+	private int number;
+	private boolean jump = false;
 
 	public Player(int xPos, int yPos, int number) {
 		super(xPos, yPos);
@@ -21,12 +21,12 @@ public class Player extends Sprite {
 	
 	private void initPlayer() {
 		if (number == 1) {
-//			loadImage("Sprite1.png");
+			//loadImage("Sprite1.png");
 			setImage(Images.player1);
 			getDimensions();
 		}
 		if (number == 2) {
-//			loadImage("Sprite2.png");
+			//loadImage("Sprite2.png");
 			setImage(Images.player2);
 			getDimensions();
 		}
@@ -40,7 +40,7 @@ public class Player extends Sprite {
 		this.jump = jump;
 	}
 	
-	public void move() {
+	void move() {
 		xPos += dx;
 		yPos += dy;
 
@@ -52,7 +52,7 @@ public class Player extends Sprite {
 		}
 	}
 
-	public void keyPressed(KeyEvent e) {
+	void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		if (number == 1) {
 			if (key == KeyEvent.VK_LEFT) {
@@ -63,23 +63,20 @@ public class Player extends Sprite {
 			}
 			if (key == KeyEvent.VK_UP) {
 				jump = true;
-				Timer engine = new Timer(1, new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						if (jump) {
-							dy = -15;
-							if (yPos <= -200) {
-								jump = !jump;
-							}
-						}
-						else if (yPos >= 0){
-							dy = 0;
-						}
-						else {
-							dy = 15;
-						}
-					}
-				});
+				Timer engine = new Timer(1, e1 -> {
+                    if (jump) {
+                        dy = -15;
+                        if (yPos <= -200) {
+                            jump = !jump;
+                        }
+                    }
+                    else if (yPos >= 0){
+                        dy = 0;
+                    }
+                    else {
+                        dy = 15;
+                    }
+                });
 				engine.start();
 			}
 		}
@@ -92,29 +89,26 @@ public class Player extends Sprite {
 			}
 			if (key == KeyEvent.VK_W) {	
 				jump = true;
-				Timer engine = new Timer(1, new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						if (jump) {
-							dy = -15;
-							if (yPos <= -200) {
-								jump = !jump;
-							}
-						}
-						else if (yPos >= 0){
-							dy = 0;
-						}
-						else {
-							dy = 15;
-						}
-					}
-				});
+				Timer engine = new Timer(1, e1 -> {
+                    if (jump) {
+                        dy = -15;
+                        if (yPos <= -200) {
+                            jump = !jump;
+                        }
+                    }
+                    else if (yPos >= 0){
+                        dy = 0;
+                    }
+                    else {
+                        dy = 15;
+                    }
+                });
 				engine.start();
 			}
 		}
 	}
 
-	public void keyReleased(KeyEvent e) {
+	void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		if (number == 1) {
 			if (key == KeyEvent.VK_LEFT) {
