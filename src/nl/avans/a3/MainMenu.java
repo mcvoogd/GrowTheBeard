a0ced496042d02cfc5cbdd26b0a85925a892fdee
Remco Korevaar <remcokorevaar@gmail.com>
2016-05-24 11:31:19 +0200

    Restyled main menu

--- src/nl/avans/a3/MainMenu.java
+++ src/nl/avans/a3/MainMenu.java
@@ -26,29 +26,24 @@ public class MainMenu extends JPanel {
     private final int MAXROTATION = 20;
     private boolean triggered = false;
 
-    public enum Mode
-    {
+    public enum Mode{
         CHOOSEPARTY, CHOOSESINGLE
     }
 
     private Mode mode = Mode.CHOOSESINGLE;
 
-    public MainMenu()
-    {
+    public MainMenu(){
         Logger.instance.log("MM001", "MainMenu entered", Logger.LogType.LOG);
-        try
-        {
+        try{
             background = ImageIO.read(new File("res/menu/background.png"));
             partyGame = ImageIO.read(new File("res/menu/party.png"));
             singleGame = ImageIO.read(new File("res/menu/single.png"));
             Logger.instance.log("MM003", "Loading images succesvol!", Logger.LogType.LOG);
-
         } catch (IOException e) {
             Logger.instance.log("MM002", "Loading images failed", Logger.LogType.ERROR);
         }
 
         rotatePartyTimer = new Timer(1000/60, e -> {
-
             if (rotation < MAXROTATION && !triggered) {
                 rotation += 1;
                 if (rotation >= MAXROTATION) {
@@ -61,23 +56,19 @@ public class MainMenu extends JPanel {
                 }
             }
         });
-
     }
 
     public void update(Graphics2D g)
     {
-
         g.drawImage(background, 0, 0, null);
-        switch(mode)
-        {
+        switch(mode){
             case CHOOSEPARTY : drawParty(g); break;
             case CHOOSESINGLE : drawSingle(g); break;
         }
     }
 
     private void drawSingle(Graphics2D g) {
-        if(!rotatePartyTimer.isRunning())
-        {
+        if(!rotatePartyTimer.isRunning()){
             rotatePartyTimer.start();
         }
         g.drawImage(partyGame, PARTYBOARDX, PARTYBOARDY, null);
@@ -86,25 +77,21 @@ public class MainMenu extends JPanel {
         g.drawImage(singleGame, EasyTransformer.rotateAroundCenterWithOffset(singleGame, rotation, 0, -singleGame.getHeight()/2, SINGLEBOARDX, SINGLEBOARDY), null);
         g.setColor(Color.DARK_GRAY);
         g.fillOval(SINGLEBOARDX + 240, SINGLEBOARDY - 10, 60, 60); //TODO spijker image.!
-
     }
 
     private void drawParty(Graphics2D g) {
-        if(!rotatePartyTimer.isRunning())
-        {
+        if(!rotatePartyTimer.isRunning()){
             rotatePartyTimer.start();
         }
-       g.drawImage(partyGame, EasyTransformer.rotateAroundCenterWithOffset(partyGame, rotation, 0, -partyGame.getHeight()/2, PARTYBOARDX, PARTYBOARDY), null);
-       g.setColor(Color.DARK_GRAY);
-       g.fillOval(PARTYBOARDX + 320, PARTYBOARDY - 10, 60, 60); //TODO spijker image.!
-       g.drawImage(singleGame, SINGLEBOARDX, SINGLEBOARDY, null);
-       g.setColor(Color.DARK_GRAY);
-       g.fillOval(SINGLEBOARDX + 240, SINGLEBOARDY - 10, 60, 60); //TODO spijker image.!
-
+        g.drawImage(partyGame, EasyTransformer.rotateAroundCenterWithOffset(partyGame, rotation, 0, -partyGame.getHeight()/2, PARTYBOARDX, PARTYBOARDY), null);
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval(PARTYBOARDX + 320, PARTYBOARDY - 10, 60, 60); //TODO spijker image.!
+        g.drawImage(singleGame, SINGLEBOARDX, SINGLEBOARDY, null);
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval(SINGLEBOARDX + 240, SINGLEBOARDY - 10, 60, 60); //TODO spijker image.!
     }
 
-    public void setMode(Mode mode)
-    {
+    public void setMode(Mode mode){
        this.mode = mode;
     }
 }
