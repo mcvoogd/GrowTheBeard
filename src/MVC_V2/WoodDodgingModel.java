256d77855e2e1bd47abad9fe7d35d43bc04510ee
Kevin van den Broek <kevin.vandenbroek@home.nl>
2016-05-30 09:04:59 +0200

    prepared first game to MVC

---
+++ src/MVC_V2/WoodDodgingModel.java
@@ -0,0 +1,18 @@
+package MVC_V2;
+
+public class WoodDodgingModel implements Model{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/MVC_V2/WoodDodgingView.java
@@ -0,0 +1,26 @@
+package MVC_V2;
+
+import java.awt.*;
+
+public class WoodDodgingView implements View {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void onModelEvent(ModelEvent event) {
+
+    }
+}
