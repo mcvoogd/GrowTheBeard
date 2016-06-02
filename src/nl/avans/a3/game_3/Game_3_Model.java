03fd3452038040cfcb6c034f22fa8aa84b86df11
HarmenL <leusinkharmen@gmail.com>
2016-06-02 10:01:22 +0200

    Added Game3 Model, Controller and View

---
+++ src/nl/avans/a3/game_3/Game_3_Model.java
@@ -0,0 +1,20 @@
+package nl.avans.a3.game_3;
+
+import nl.avans.a3.mvc_interfaces.Model;
+
+public class Game_3_Model implements Model{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/nl/avans/a3/game_3/Game_3_View.java
@@ -0,0 +1,35 @@
+package nl.avans.a3.game_3;
+
+import nl.avans.a3.event.ModelEvent;
+import nl.avans.a3.mvc_interfaces.View;
+
+import java.awt.*;
+
+public class Game_3_View implements View{
+
+    Game_3_Model gameModel;
+
+    public Game_3_View(Game_3_Model gameModel){
+        this.gameModel = gameModel;
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void onModelEvent(ModelEvent event) {
+
+    }
+}
