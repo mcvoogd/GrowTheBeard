ab8e76b67edc21370b041fc479c1eeead40ae6f3
FloboAvans <fbelzeli@student.avans.nl>
2016-06-09 16:04:33 +0200

    added platform stuff

--- src/nl/avans/a3/game_2/Game_2_Model.java
+++ src/nl/avans/a3/game_2/Game_2_Model.java
@@ -27,12 +27,18 @@ public class Game_2_Model implements Model
 
     final int BLOCK_WIDTH = 160;
     final int BLOCK_HEIGHT = 50;
-    final float BLOCK_FALL_SPEED = -0.5f;
+    final float BLOCK_FALL_SPEED = -3;
+
+    final int WORLD_HEIGHT_LOW_BOUND = (-PLAYER_HEIGHT-BLOCK_HEIGHT)*2;
 
     final int GROUND_LEFT_X = -500;
     final int GROUND_LEFT_Y = -245;
     final int GROUND_LEFT_WIDTH = 1000;
     final int GROUND_LEFT_HEIGHT = 470;
+
+    final int PLAYER_SPAWN_Y = GROUND_LEFT_Y+GROUND_LEFT_HEIGHT+1;
+    final int PLAYER_SPAWN_X_1 = PlAYER_WIDTH;
+    final int PLAYER_SPAWN_X_2 = (int)(PlAYER_WIDTH*3);
 
     final int GROUND_RIGHT_X = 1400;
     final int GROUND_RIGHT_Y = -275;
@@ -40,6 +46,8 @@ public class Game_2_Model implements Model
     final int GROUND_RIGHT_HEIGHT = 500;
 
     final int BLOCK_SPAWN_Y = WORLD_HEIGHT_BOUND+BLOCK_HEIGHT;
+    final int BLOCK_SPAWN_Y_SECTION_COUNT = 9;
+    final int BLOCK_SPAWN_Y_SECTION = (BLOCK_SPAWN_Y-WORLD_HEIGHT_LOW_BOUND)/BLOCK_SPAWN_Y_SECTION_COUNT;
     final int BLOCK_SPAWN_X_BASE = GROUND_LEFT_X+GROUND_LEFT_WIDTH;
     final int BLOCK_SPAWN_X_SECTION = (GROUND_RIGHT_X-BLOCK_SPAWN_X_BASE)/3;
     final int BLOCK_SPAWN_X_1 = BLOCK_SPAWN_X_BASE+BLOCK_SPAWN_X_SECTION*0+((BLOCK_SPAWN_X_SECTION-BLOCK_WIDTH)/2);
@@ -67,18 +75,19 @@ public class Game_2_Model implements Model
         final int id;
         boolean jump = false;
         float movX = 0;
-        Collidiable platform = null;
+        Platform platform = null;
+        final float spawnX,  spawnY;
 
         Player(int id, float x, float y)
         {
             super(PlAYER_WIDTH, PLAYER_HEIGHT);
             this.id = id;
-            this.x = x;
-            this.y = y;
+            spawnX = this.x = x;
+            spawnY= this.y = y;
             ModelHandler.instance.onModelEvent(new G2_NewObject(id, true, x, y));
         }
 
-        final int JUMP_DURATION = 75;
+        final int JUMP_DURATION = 60;
         final double JUMP_HEIGHT = 200;
         int jumpTicks = JUMP_DURATION;
 
@@ -117,7 +126,7 @@ public class Game_2_Model implements Model
                 }
                 for (Platform platform : platforms)
                     if (getBounds().intersects(platform.getBounds())) {
-                        if (isGoodIntersect(platform.getBounds())) {
+                        if (isGoodIntersect(platform.getBounds()) && platform.state == PlatformState.FALLING) {
                             this.platform = platform;
                             state = PlayerState.ON_PLATFORM;
                             ModelHandler.instance.onModelEvent(new G2_PlayerStateChange(G2_PlayerStateChange.State.WALK, id));
@@ -141,6 +150,11 @@ public class Game_2_Model implements Model
             }
             else
             {
+                if (platform.state == PlatformState.REMOVE)
+                {
+                    state = PlayerState.JUMPING;
+                    update();
+                }
                 x += movX * 5;
                 y = platform.y + platform.height;
                 if (x + width/2 < platform.x || platform.x + platform.width < x+width/2)
@@ -152,7 +166,14 @@ public class Game_2_Model implements Model
             }
 
             x = MathExtended.clamp(x, 0, WORLD_WIDTH_BOUND-PlAYER_WIDTH);
-            y = MathExtended.clamp(y, -100, WORLD_HEIGHT_BOUND-PLAYER_HEIGHT);
+            y = MathExtended.clamp(y, WORLD_HEIGHT_LOW_BOUND*2, WORLD_HEIGHT_BOUND-PLAYER_HEIGHT/2);
+            if (y <= WORLD_HEIGHT_LOW_BOUND)
+            {
+                state = PlayerState.JUMPING;
+                x = spawnX;
+                y = spawnY;
+            }
+
             ModelHandler.instance.onModelEvent(new G2_ObjectMove(id, true, x, y));
             jump = false;
         }
@@ -167,7 +188,7 @@ public class Game_2_Model implements Model
     protected class Platform extends Collidiable
     {
         float fallingSpeed;
-        PlatformState state = PlatformState.FALLING;
+        public PlatformState state = PlatformState.FALLING;
         final boolean canMoveTrough;
 
         Platform(float x, float y, float width, float height, float fallingSpeed, boolean canMoveTrough) {
@@ -179,8 +200,14 @@ public class Game_2_Model implements Model
         }
 
         public void update() {
+            if (state == PlatformState.REMOVE)
+            {
+                state = PlatformState.FALLING;
+                y = BLOCK_SPAWN_Y;
+            }
+
             y += fallingSpeed;
-            if (y < -height)
+            if (y <= WORLD_HEIGHT_LOW_BOUND)
                 state = PlatformState.REMOVE;
         }
     }
@@ -212,12 +239,26 @@ public class Game_2_Model implements Model
 
     @Override
     public void start() {
-        for (int i = 0; i < PLAYER_COUNT; i++)
-            players[i] = new Player(i, 100+75*i, 700);
+        players[0] = new Player(0, PLAYER_SPAWN_X_1, PLAYER_SPAWN_Y);
+        players[1] = new Player(1, PLAYER_SPAWN_X_2, PLAYER_SPAWN_Y);
+
         platforms.add(new Platform(GROUND_LEFT_X, GROUND_LEFT_Y, GROUND_LEFT_WIDTH, GROUND_LEFT_HEIGHT, 0, false));
         platforms.add(new Platform(GROUND_RIGHT_X, GROUND_RIGHT_Y, GROUND_RIGHT_WIDTH, GROUND_RIGHT_HEIGHT, 0, false));
 
-        platforms.add(new Woodblock(600, 400));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*9));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_3, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*8));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_1, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*7));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*7));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*6));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_3, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*6));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*5));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_1, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*4));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_3, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*4));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*3));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_2, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*2));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_3, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION*2));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_1, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION));
+        platforms.add(new Woodblock(BLOCK_SPAWN_X_3, WORLD_HEIGHT_LOW_BOUND+BLOCK_SPAWN_Y_SECTION));
     }
 
     Random rand = new Random(System.currentTimeMillis());
