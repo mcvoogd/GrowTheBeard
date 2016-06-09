23c8e1c525020831ed4c8a3b3aa2bcc644bcfb3a
HarmenL <leusinkharmen@gmail.com>
2016-06-09 15:21:02 +0200

    GROW THE BEARD IN THE GAME

---
+++ src/nl/avans/a3/util/Beard.java
@@ -0,0 +1,10 @@
+package nl.avans.a3.util;
+
+/**
+ * Created by Harmen on 9-6-2016.
+ */
+public class Beard {
+
+    public static int beardPlayer1 = 0;
+    public static int beardPlayer2 = 0;
+}
