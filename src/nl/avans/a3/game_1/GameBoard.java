8ddf78708e00a2e7b2856e05c0567a1119393343
Remco Korevaar <remcokorevaar@gmail.com>
2016-06-10 14:42:17 +0200

    Added sound effects to game 1

--- src/nl/avans/a3/game_1/GameBoard.java
+++ src/nl/avans/a3/game_1/GameBoard.java
@@ -1,12 +1,11 @@
 package nl.avans.a3.game_1;
 import nl.avans.a3.event.NewModel;
+import nl.avans.a3.game_1.DummyMVC.DummyModel;
+import nl.avans.a3.game_1.DummyMVC.DummyView;
 import nl.avans.a3.main_menu.MainMenuModel;
 import nl.avans.a3.mvc_handlers.ModelHandler;
 import nl.avans.a3.party_mode_handler.PartyModeHandler;
-import nl.avans.a3.util.Beard;
-import nl.avans.a3.util.EasyTransformer;
-import nl.avans.a3.util.ResourceHandler;
-import nl.avans.a3.util.WiimoteHandler;
+import nl.avans.a3.util.*;
 import nl.avans.a3.game_1.Util.Images;
 
 import java.awt.Color;
@@ -79,18 +78,22 @@ public class GameBoard extends JPanel implements ActionListener {
 	private boolean playerCollision = false;
 
 	private final int WOODBLOCK_START_COUNT = 3;
-    private boolean notTriggerd = true;
-
-
-
-	private boolean preScreen;
+    private boolean preScreen;
 
 	private int beardCounter;
 	private BufferedImage playerWin1, playerWin2;
 	private BufferedImage[] beards = new BufferedImage[6];
+    
+    private ArrayList<String> hitSounds = new ArrayList<>();
+    private SoundPlayer soundPlayer;
 
     public GameBoard(WiimoteHandler wiimoteHandler) {
 		this.wiimoteHandler = wiimoteHandler;
+        hitSounds.add("res/music/wood_hit_1");
+        hitSounds.add("res/music/wood_hit_2");
+        hitSounds.add("res/music/wood_hit_3");
+        hitSounds.add("res/music/wood_hit_4");
+        soundPlayer = new SoundPlayer(hitSounds);
 		initGameBoard();
 	}
 	
@@ -121,12 +124,12 @@ public class GameBoard extends JPanel implements ActionListener {
 
         initWoodBlocks();
 
-			endTimer = new Timer(time * 1000, e -> inGame = false);
-			timeLeft = new Timer(1000, e -> {
-				time--;
-				scorePlayer1++;
-				scorePlayer2++;
-			});
+		endTimer = new Timer(time * 1000, e -> inGame = false);
+		timeLeft = new Timer(1000, e -> {
+			time--;
+			scorePlayer1++;
+			scorePlayer2++;
+		});
 		cutPlayerImage();
 		cutBeards();
 
@@ -206,7 +209,6 @@ public class GameBoard extends JPanel implements ActionListener {
         }
 		Toolkit.getDefaultToolkit().sync();
 	}
-
 
 	private void drawPlayers(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
@@ -316,6 +318,7 @@ public class GameBoard extends JPanel implements ActionListener {
 				scorePlayer1-=2;
 				hit = true;
 				rumble(0);
+                soundPlayer.playRandomOnce(10);
 				if (scorePlayer1 < 0) {
 					scorePlayer1 = 0;
 				}
@@ -324,6 +327,7 @@ public class GameBoard extends JPanel implements ActionListener {
 				scorePlayer2-=2;
 				hit = true;
 				rumble(1);
+                soundPlayer.playRandomOnce(10);
 				if (scorePlayer2 < 0) {
 					scorePlayer2 = 0;
 				}
@@ -333,10 +337,9 @@ public class GameBoard extends JPanel implements ActionListener {
 				for(int i = 0; i < 10; i++){
 					particles.add(new Particle(woodBlock.getX(), woodBlock.getY(), i*36));
 				}
+				
 				it.remove();
-
 			}
-
 		}
 		while(woodBlocks.size() < 4){
 			woodBlocks.add(new WoodBlock(getRandomInt(10, 1880), -1000, -getRandom(2,1),  woodImages[getRandom(2,0)], getRandom(360,0)));
