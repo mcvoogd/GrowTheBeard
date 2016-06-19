026e6bc2c1bbbd3740fd2f4335a7d49e11ae0615
Kevintjeb <Kevin.vandenbroek@home.nl>
2016-06-20 00:54:20 +0200

    added waterfall to prescreen flobo

--- src/nl/avans/a3/game_2/Game_2_View_PreScreen.java
+++ src/nl/avans/a3/game_2/Game_2_View_PreScreen.java
@@ -15,21 +15,25 @@ public class Game_2_View_PreScreen implements View {
     BufferedImage[] images;
     int counter = 0;
     final int UPDATES_PER_INDEX = 20;
-
+    private BufferedImage[] background;
     public Game_2_View_PreScreen(Game_2_Model model) {this.model = model;}
 
     @Override
     public void start() {
+        BufferedImage image2 = ResourceHandler.getImage("res/images_game2/background.png");
         BufferedImage image = ResourceHandler.getImage("res/images_game2/instructions.png");
         images = new BufferedImage[3];
-        for (int i =0; i < 3; i++)
+        background = new BufferedImage[3];
+        for (int i = 0; i < 3; i++)
         {
             images[i] = image.getSubimage(0, 1080 * i, 1920, 1080);
+            background[i] = image2 != null ? image2.getSubimage(0, 1080 * i, 1920, 1080) : null;
         }
     }
 
     @Override
     public void draw(Graphics2D g) {
+        g.drawImage(background[0], 0, 0, null);
         g.drawImage(images[counter/UPDATES_PER_INDEX], 0, 0, null);
 
         if (++counter >= UPDATES_PER_INDEX*images.length)
