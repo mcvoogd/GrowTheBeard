fc81db8765709f548ce1cad5f895e8f1175c4861
HarmenL <leusinkharmen@gmail.com>
2016-06-20 02:11:37 +0200

    moved to view

--- src/nl/avans/a3/game_2/Game_2_Model.java
+++ src/nl/avans/a3/game_2/Game_2_Model.java
@@ -75,7 +75,7 @@ public class Game_2_Model implements Model {
     public enum ModelState{PRE_SCREEN, GAME, WINSCREEN}
     private ModelState state = ModelState.PRE_SCREEN;
     public ModelState getState() {return state;}
-    private SoundPlayer scoreMusic;
+
 
     private class Collidable{
         float x, y;
@@ -283,7 +283,7 @@ public class Game_2_Model implements Model {
     public Game_2_Model()
     {
         platforms = new ArrayList<>();
-        scoreMusic = new SoundPlayer("res/music/theme_song.wav");
+
     }
 
     private boolean hasStarted = false;
@@ -296,7 +296,6 @@ public class Game_2_Model implements Model {
         new Timer(time * 1000, e -> {
             inGame = false;
             state = ModelState.WINSCREEN;
-            scoreMusic.loop(20);
             if (getScores().getKey() > getScores().getValue()) Beard.beardPlayer1++;
             else if (getScores().getKey() < getScores().getValue()) Beard.beardPlayer2 ++;
         }).start();
@@ -340,7 +339,7 @@ public class Game_2_Model implements Model {
 
     @Override
     public void close() {
-            scoreMusic.stop();
+
     }
 
     public void setGameStart()
